6b3694a8428fbc6cc986b2d40a9073f56e4eb87d
Romain Andres <romain.andres@etu.unicaen.fr>
2024-04-05 12:55:49 +0200

    changement button to Button ui platform

--- Viewers-3.7.0/extensions/default/src/ViewerLayout/ViewerHeader.tsx
+++ Viewers-3.7.0/extensions/default/src/ViewerLayout/ViewerHeader.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 
-import { ErrorBoundary, UserPreferences, AboutModal, Header, useModal } from '@ohif/ui';
-import i18n from '@ohif/i18n';
 import { hotkeys } from '@ohif/core';
+import i18n from '@ohif/i18n';
+import { AboutModal, ErrorBoundary, Header, UserPreferences, useModal } from '@ohif/ui';
 import { useAppConfig } from '@state';
 import Toolbar from '../Toolbar/Toolbar';
 
