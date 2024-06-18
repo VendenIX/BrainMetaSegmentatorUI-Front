8692d6018056f3f692aaa520da5a9b41dfc0a4ff
Romain ANDRES <romain.andres@etu.unicaen.fr>
2024-06-18 23:02:30 +0200

    suppression du chargement par lots pour permettre l'ajout des infos des rtstruct à la bdd sqlite

--- Viewers-3.7.0/platform/app/src/routes/WorkList/StudyUploadPopup.tsx
+++ Viewers-3.7.0/platform/app/src/routes/WorkList/StudyUploadPopup.tsx
@@ -1,5 +1,5 @@
 import { Modal, ProgressLoadingBar } from '@ohif/ui';
-import React, { useState,useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import DragAndDrop from './DragAndDrop';
 
 const StudyUploadPopup = ({ isOpen, onClose, onComplete }) => {
@@ -15,23 +15,22 @@ const StudyUploadPopup = ({ isOpen, onClose, onComplete }) => {
     };
   }, []);
 
-  const handleFilesDrop = (files) => {
-    setIsUploading(true);
-    for (let i = 0; i < files.length; i++) {
-      uploadFileToServer(files[i], files.length, i).then(() => {
-        if (i === files.length - 1) {
-          if (isMounted.current) {
-            onComplete();  // Appel de onComplete après le dernier fichier
-            setIsUploading(false);
-          }
-        }
-      });
-    }
+  const handleFilesDrop = async (files) => {
+    await handleFileUpload(Array.from(files));
   };
 
-  const uploadFileToServer = async (file, totalFiles, currentIndex) => {
+  const handleFilesSelection = async (event) => {
+    const files = event.target.files;
+    await handleFileUpload(Array.from(files));
+  };
+
+  const handleFileUpload = async (files) => {
+    setIsUploading(true);
+
     const formData = new FormData();
-    formData.append('file', file);
+    files.forEach(file => {
+      formData.append('files[]', file);
+    });
 
     try {
       const response = await fetch('http://localhost:5000/uploadDicom', {
@@ -44,25 +43,14 @@ const StudyUploadPopup = ({ isOpen, onClose, onComplete }) => {
       }
 
       const result = await response.json();
-      const updatedIndex = currentIndex + 1;
       if (isMounted.current) {
-        setUploadProgress((updatedIndex / totalFiles) * 100);
-        setFilesCounter(`${updatedIndex}/${totalFiles}`);
-      }
-
-      if (currentIndex === totalFiles - 1) {
-        setTimeout(() => {
-          if (isMounted.current) {
-            setUploadProgress(null);
-            setFilesCounter(null);
-          }
-        }, 2000);
+        onComplete();
+        setIsUploading(false);
       }
     } catch (error) {
-      console.error("Erreur lors de l'envoi du fichier:", error);
+      console.error("Erreur lors de l'envoi des fichiers:", error);
       if (isMounted.current) {
-        setUploadProgress(null);
-        setFilesCounter(null);
+        setIsUploading(false);
       }
     }
   };
@@ -89,6 +77,57 @@ const StudyUploadPopup = ({ isOpen, onClose, onComplete }) => {
       </div>
     </Modal>
   );
+};
+
+const styles = {
+  fileInput: {
+    display: 'block',
+    marginTop: '10px',
+    padding: '10px',
+    border: '1px solid #30363D',
+    borderRadius: '4px',
+    backgroundColor: '#0D1117',
+    color: '#CCCCCC',
+    fontFamily: 'Arial, sans-serif',
+  },
+  container: {
+    backgroundColor: '#1E1E1E', // un fond plus foncé
+    color: '#CCCCCC', // texte gris clair pour contraste
+    padding: '20px',
+    fontFamily: 'Arial, sans-serif',
+    borderRadius: '8px', // Bords arrondis
+    boxShadow: '0 4px 8px rgba(0,0,0,0.1)', // ombre légère pour du relief
+    marginTop: '20px',
+    overflowX: 'auto' // pour la gestion du débordement sur l'axe X
+  },
+  title: {
+    color: '#58A6FF', // bleu clair
+    textAlign: 'center',
+    marginBottom: '20px' // espace sous le titre
+  },
+  table: {
+    width: '100%',
+    borderCollapse: 'collapse',
+    color: '#CCCCCC' // texte de la table en gris clair
+  },
+  header: {
+    backgroundColor: '#0D1117', // fond d'en-tête plus foncé
+    padding: '10px',
+    textAlign: 'left',
+    color: '#58A6FF', // texte de l'en-tête en bleu
+    borderBottom: '2px solid #30363D' // ligne de séparation
+  },
+  row: {
+    backgroundColor: '#161B22', // alternance de couleur pour les rangées
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: '#30363D' // couleur au survol
+    }
+  },
+  cell: {
+    padding: '10px',
+    borderBottom: '1px solid #30363D' // ligne de séparation plus subtile
+  }
 };
 
 export default StudyUploadPopup;
